0a1a91bc885863c45f7adc31f78946e020b88d77
RaviKVerma <ravikrverma2002@gmail.com>
2026-09-24 01:04:43 +0530

    ClassicModels SQL queries and screenshots

---
+++ Classic Models.sql
@@ -0,0 +1,92 @@
+USE classicmodels;
+
+SHOW TABLES;
+
+SELECT * FROM customers;
+
+-- CUSTOMER ANALYSIS 
+
+SELECT * FROM customers 
+WHERE creditlimit >= 100000;
+
+SELECT * FROM customers 
+WHERE country = 'USA' AND creditlimit >= 50000
+ORDER BY creditlimit DESC;
+
+SELECT * FROM customers
+WHERE customername LIKE 'A%';
+
+SELECT * FROM customers
+WHERE customername LIKE '%Mini%';
+
+SELECT * FROM customers 
+WHERE country IN ('France','USA','Australia');
+
+SELECT * FROM customers
+ORDER BY customername ASC
+LIMIT 10;
+
+SELECT * FROM customers
+ORDER BY customername ASC
+LIMIT 10,10;
+
+SELECT country, COUNT(*) 
+AS customer_count FROM customers
+GROUP BY country;
+
+SELECT country, COUNT(*) 
+AS customer_count FROM customers
+GROUP BY country
+HAVING customer_count >= 5;
+
+SELECT customername, creditlimit 
+FROM customers
+WHERE creditlimit = (SELECT MAX(creditlimit) 
+FROM customers);
+
+-- PRODUCT ANALYSIS 
+
+SELECT * FROM products;
+
+SELECT productname,productline,quantityinstock,buyprice
+FROM products;
+
+SELECT * FROM products
+WHERE quantityInStock < 1000;
+
+SELECT * FROM products
+WHERE productname LIKE '%Porsche%';
+
+SELECT productLine, COUNT(*)
+AS productline_count FROM products
+GROUP BY productLine;
+
+SELECT productLine, MAX(buyPrice),
+MIN(buyPrice) FROM products
+GROUP BY productLine;
+
+SELECT * FROM payments;
+ 
+SELECT COUNT(*) FROM payments
+WHERE paymentdate < "2003-02-16";
+
+SELECT customernumber, COUNT(*) AS PaymentCount
+FROM payments
+GROUP BY customernumber
+ORDER BY PaymentCount DESC;
+
+SELECT customernumber,
+SUM(amount) AS TotalPayment
+FROM payments
+GROUP BY customerNumber
+ORDER BY TotalPayment DESC;
+
+SELECT customerNumber, 
+ROUND(AVG(amount),2) AS AveragePayment
+FROM payments
+GROUP BY customerNumber
+ORDER BY AveragePayment DESC;
+
+SELECT customerNumber, MAX(amount),
+MIN(amount) FROM payments
+GROUP BY customerNumber;
